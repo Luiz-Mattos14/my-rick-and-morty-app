64c9f938540156af286feda56c7f74c8544fb0f9
Luiz Mattos <luiz.esmattos@gmail.com>
2025-01-13 04:05:30 -0300

    Feat: integration sass

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")], // Adiciona a pasta "styles" ao caminho
+  },
 };
 
 export default nextConfig;
